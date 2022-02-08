625a8d9ee47a3ee376a76adfce01f09d5546fb30
Johorin <hoyuak2145@docomo.ne.jp>
2022-02-08 19:31:28 +0900

    カリキュラム09-2.最後まで

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -16,7 +16,7 @@
             <div class="content__post">
                 <h3>本文</h3>
                 <p>{{ $post->body }}</p>
-                <a href="">{{ $post->category->name }}</a>
+                <a href="/categories/{{ $post->category->id }}">{{ $post->category->name }}</a>
             </div>
         </div>
         <p class="edit">[<a href="/posts/{{ $post->id }}/edit">edit</a>]</p>
